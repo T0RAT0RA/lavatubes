fc16a0a7ad7ec9b0da1fb024aa215ae2add873e0
sreiss <stephane.reiss@alayacare.com>
2015-04-11 18:06:40 -0400

    Fixed compiled js.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 define("io", ["/socket.io/socket.io.js"], function(io){ return io; });
-define(["lib/class", "underscore", "jquery", "jquery-ui", "less"], function(){
+require(["lib/class", "underscore", "jquery", "jquery-ui", "less"], function(){
     $(document).ready(function(){
         require(["modules/game"]);
     });
